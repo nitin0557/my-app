4ce70ca810484f32b894b7d8e26a7d4f45133b27
Nitin Kumar Singh <106854866+nitin0557@users.noreply.github.com>
2022-06-04 14:20:27 +0530

    Updates

---
+++ precache-manifest.326d171144266bea4f0995759133f14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e2040e1aa3710272da",
+    "url": "/my-app/static/css/main.c653a315.chunk.css"
+  },
+  {
+    "revision": "38e2040e1aa3710272da",
+    "url": "/my-app/static/js/main.38e2040e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/my-app/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "58153c17cab40b2cdbd65c3806e69439",
+    "url": "/my-app/index.html"
+  }
+];
